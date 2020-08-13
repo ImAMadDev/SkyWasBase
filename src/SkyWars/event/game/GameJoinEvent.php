76bd943caaa0e3e647f8c4d9d2df11c6f4e8ba06
Trasher <30436794+Kaneki-chi@users.noreply.github.com>
2020-08-13 16:55:13 -0500

    Create GameJoinEvent.php

---
+++ src/SkyWars/event/game/GameJoinEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SkyWars\event\game;
+
+use SkyWars\SkyWars;
+
+use SkyWars\game\Game;
+
+use pocketmine\event\plugin\PluginEvent;
+
+use pocketmine\event\Cancellable;
+
+use pocketmine\Player;
+
+class GameJoinEvent extends PluginEvent implements Cancellable {
+
+	protected $player;	protected $game;
+
+	public function __construct(SkyWars $plugin, Player $player, Game $game){
+
+		parent::__construct($plugin);
+
+		$this->player = $player;
+
+		$this->game = $game;
+
+	}
+
+	public function getPlayer(){
+
+		return $this->player;
+
+	}
+
+	public function getGame(){
+
+		return $this->game;
+
+	}
+
+}
